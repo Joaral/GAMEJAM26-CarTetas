5d0cbd85eb115613c50d643dfd27205ca3aa083d
Joaral <joan.ariza@estudiant.enti.cat>
2026-01-20 16:47:35 +0100

    Donde estan los penes

--- CarTetas/Assets/MORITA/Scripts/cardsHandler.cs
+++ CarTetas/Assets/MORITA/Scripts/cardsHandler.cs
@@ -7,6 +7,8 @@ public class cardsHandler : MonoBehaviour
     public GameManager gameManager;
     public List<GameObject> cards;
     public Transform spawnPoint;
+    public GameObject sobre;
+    public Animator animator;
 
     public GameObject currentCard;
     public bool isCardAlive;
@@ -18,6 +20,8 @@ public class cardsHandler : MonoBehaviour
         input = new InputSystem_Actions();
         input.Enable();
 
+        animator = sobre.GetComponent<Animator>();
+
         SpawnCard();
     }
 
@@ -25,27 +29,33 @@ public class cardsHandler : MonoBehaviour
     {
         if (gameManager.gameOver) return;
 
-        //if (input.Player.Next.WasPressedThisFrame())
-        //{
-        if (!isCardAlive)
+        if (input.Player.Next.WasPressedThisFrame())
+        {
+            if (!isCardAlive)
             {
                 SpawnCard();
             }
-        //}
+        }
     }
 
     void SpawnCard()
     {
+        Instantiate(sobre, new Vector3 (0, 0.3f, -3), Quaternion.Euler (0,-90,-90));
+        animator.SetBool("open", true);
+        
         if (gameManager.gameOver) return;
 
         int cardIndex = Random.Range(0, cards.Count);
         currentCard = Instantiate(cards[cardIndex], spawnPoint.position, spawnPoint.rotation);
+        
         isCardAlive = true;
         gameManager.SetCurrentCard(currentCard);
     }
 
     public void CardResolved()
     {
+        Destroy(sobre);
+        animator.SetBool("open", false);
         isCardAlive = false;
     }
 }
